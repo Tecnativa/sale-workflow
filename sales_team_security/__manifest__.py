9a10822e6063229b414f7313001a1be388173c54
Cesar Andres Sanchez <cesar.sanchez@tecnativa.com>
2022-06-02 23:34:10 +0200

    [MIG] sales_team_security: Migration to 15.0

--- sales_team_security/__manifest__.py
+++ sales_team_security/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Sales documents permissions by channels (teams)",
     "summary": "New group for seeing only sales channel's documents",
-    "version": "14.0.2.0.0",
+    "version": "15.0.1.0.0",
     "category": "Sales",
     "website": "https://github.com/OCA/sale-workflow",
     "author": "Tecnativa, Odoo Community Association (OCA)",
@@ -12,8 +12,12 @@
     "installable": True,
     "development_status": "Production/Stable",
     "maintainers": ["pedrobaeza", "ivantodorovich"],
-    "depends": ["sales_team"],
-    "data": ["security/sales_team_security.xml", "views/res_partner_view.xml"],
+    "depends": ["sales_team", "sale", "crm"],
+    "data": [
+        "security/sales_team_security.xml",
+        "security/sales_team_security_rules.xml",
+        "views/res_partner_view.xml",
+    ],
     "post_init_hook": "post_init_hook",
     "uninstall_hook": "uninstall_hook",
 }
